f1fd829ce415480c47161d37fa3e8ab977ee5ede
Rahul <katyayan.rahul@gmail.com>
2014-12-24 14:26:46 -0500

    Created GitHub repository and added codes for the Junk Yard exercise

---
+++ codes/changed_image.R
@@ -0,0 +1,35 @@
+## This code is reprodcued verbatim from from http://shinyapps.stat.ubc.ca/r-graph-catalog/
+library(ggplot2)
+library(gridExtra)
+
+this_base <- "fig07-27_invoice-data-filling-the-scale-line-rectangle"
+
+my_data <- data.frame(invoices = c(1986, 1989, 1990, 1991, 1993), 
+                      year = c(12, 11.8, 11.6, 11.4, 10.3))
+
+p <- ggplot(my_data, aes(x = invoices, y = year)) +
+  geom_point(size = 4,color="#31a354") +
+  geom_line(size = 1) + 
+  scale_x_continuous(breaks = seq(1986, 1993, 1), limits = c(1985, 1994), 
+                     expand = c(0, 0)) +
+  scale_y_continuous(breaks = seq(0, 14, 2), limits = c(0, 14), 
+                     expand = c(0, 0)) +
+  ylab("Number of Invoices per Hour") + xlab("Years")+
+  # ggtitle("Substantial Drop\nIn Invoices Processed per Hour") +
+  theme_bw() +
+  theme(panel.grid.major.y = element_line(colour = "grey60"),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.background =element_blank() ,
+        plot.title = element_text(size = rel(0.9)))
+
+p <- 
+  arrangeGrob(p, main = textGrob(
+    "Fig 7.27 Invoice Data: \n Filling the Scale-Line Rectangle",
+    gp = gpar(lineheight = 1, fontface = "bold")))
+
+p
+
+ggsave(paste0("./figures/improvedimage", ".png"), p, width = 6, height = 4)
+
+## note: my_data is estimated
---
+++ writings/exercisechangefigure.md
@@ -0,0 +1,16 @@
+The following figure is selected from `http://shinyapps.stat.ubc.ca/r-graph-catalog/`
+
+
+I would like to change following things in the above figure:
+
++ Remove background colour, since it does not add any information to visualization
++ Change colour of grid lines from black to grey.
++ Change grid lines from dashed to regular line. Since, dashed lines are adding unnecessary negative spaces, without adding any new information. 
++ Increase the thickness of line.
++ Colour code the points to highlight transition years.
++ Remove the description of graph (Not sure though.).
++ Add label to x-axis and tick marks on each axis.
+
+The source codes are available on GitHub: https://github.com/r2rahul/uda-intro-vis.git
+
+This is the revised figure. 
