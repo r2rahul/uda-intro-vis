2d94a7c9e132aa0757b1849790f763f7c08bfdac
Rahul <katyayan.rahul@gmail.com>
2015-01-11 19:15:51 -0500

    Added proportion data for plotting.

--- codes/prepare_data_project.R
+++ codes/prepare_data_project.R
@@ -11,7 +11,19 @@ temp[temp$Survived==1,1]<-"Alive"
 temp[temp$Embarked=="C",2]<-"Cherbourg"
 temp[temp$Embarked=="Q",2]<-"Queenstown"
 temp[temp$Embarked=="S",2]<-"Southampton"
+a<-temp[1,3]/(temp[1,3]+temp[2,3])
+b<-temp[2,3]/(temp[1,3]+temp[2,3])
+c<-temp[3,3]/(temp[3,3]+temp[4,3])
+d<-temp[4,3]/(temp[3,3]+temp[4,3])
+e<-temp[5,3]/(temp[5,3]+temp[6,3])
+f<-temp[6,3]/(temp[5,3]+temp[6,3])
+temp$ratio<-c(a,b,c,d,e,f)
 row.names(temp)<-NULL
+for (j in  seq(1,nrow(temp),by=2)){
+  buffer<-temp[j,]
+  temp[j,]<-temp[j+1,]
+  temp[j+1,]<-buffer
+}
 ## Output the data for dimple.js
 fpath<-"data/projectdata.txt"
 write.table(temp,file=fpath,sep="\t",row.names=FALSE,quote = FALSE)
